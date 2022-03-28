990c34319848f76dca5173cee286ee000de79aea
kausarahamed <galibhasan517@gmail.com>
2022-03-28 15:43:54 +0600

    secend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 import "./App.css";
+import CardGroup2 from "./Components/cardGroup2/CardGroup2";
 
 function App() {
   return (
     <div className="App">
       <h1>Using react bootstrap</h1>
       <Button variant="danger">My Button</Button>
+      <br />
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+      <CardGroup2></CardGroup2>
     </div>
   );
 }
